158e0d4ce5915a352eddcee427870978580d2ba9
aAAAAAAAAAAdam <91439875+aAAAAAAAAAAdam@users.noreply.github.com>
2021-11-11 14:26:39 +0000

    Initial commit

---
+++ Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisionsX : MonoBehaviour{
+    //Destroys dog when the ball is caught
+    private void OnTriggerEnter(Collider other){
+        Destroy(gameObject);
+    }
+}
